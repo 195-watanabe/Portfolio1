b46a3619d245670834c5c245c93b16ef96d523b8
195-watanabe <19n.oshigoto@gmail.com>
2021-08-08 22:38:49 +0900

    first commit

---
+++ JavaScriptBasics/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{  
+
+document.querySelector('button').addEventListener('click', () => {
+  const li = document.createElement('li');
+  const text = document.querySelector('input');
+  li.textContent = text.value;
+  document.querySelector('ul').appendChild(li);
+});
+
+}
